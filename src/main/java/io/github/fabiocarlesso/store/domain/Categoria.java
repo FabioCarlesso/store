386c51633ff3d17f95fa002b22544fadc4eb3810
Fabio Carlesso <fabio.carlesso@gmail.com>
2022-01-31 14:13:44 -0300

    Testando classe de dominio Categoria

---
+++ src/main/java/io/github/fabiocarlesso/store/domain/Categoria.java
@@ -0,0 +1,16 @@
+package io.github.fabiocarlesso.store.domain;
+
+import java.io.Serializable;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Categoria implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private Integer id;
+    private String nome;
+
+}
