53383386e8e1f21e8b5037c67fec2e17cb70d5e4
Fabio Carlesso <fabio.carlesso@gmail.com>
2022-02-01 14:59:00 -0300

    Criado classes de dominio Cidade e Estado

--- src/main/java/io/github/fabiocarlesso/store/StoreApplication.java
+++ src/main/java/io/github/fabiocarlesso/store/StoreApplication.java
@@ -8,6 +8,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import io.github.fabiocarlesso.store.domain.Categoria;
+import io.github.fabiocarlesso.store.domain.Cidade;
+import io.github.fabiocarlesso.store.domain.Estado;
 import io.github.fabiocarlesso.store.domain.Produto;
 import io.github.fabiocarlesso.store.repositories.CategoriaRepository;
 import io.github.fabiocarlesso.store.repositories.ProdutoRepository;
@@ -44,6 +46,17 @@ public class StoreApplication implements CommandLineRunner {
 
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+
+		Cidade c1 = new Cidade(null, "Uberlandia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2, c3));
+
 	}
 
 }
